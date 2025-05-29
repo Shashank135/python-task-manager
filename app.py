2a8e4dfe4ab0d50cbabbced1a87a58c720e1c3ec
Shashank135 <shashankhu13@gmail.com>
2025-05-29 09:39:20 +0530

    Initial commit: Basic Streamlit app and requirements

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Simple Task Manager", layout="centered")
+    st.title("My Simple Python Task Manager")
+
+    st.write("Welcome to your task manager!")
+    st.info("This is where your tasks will appear. We'll add functionality soon.")
+
+if __name__ == "__main__":
+    main()
